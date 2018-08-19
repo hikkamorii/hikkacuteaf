29dd685fe48a43b1af0c6185566ff3dc60bd252e
Daniel Hikkamorii <zcy@inbox.lv>
2018-08-20 02:17:59 +0300

    Added .Xressources

--- update.sh
+++ update.sh
@@ -7,7 +7,8 @@ then
 	if [ -z "$2" ]
 	then
 		echo 'Sowwwy I disappointed you! ( ´•̥̥̥ω•̥̥̥` ) Restoring backup from previous backup.'
-			cd ./backup/backup-$PREV	
+			cd ./backup/backup-$PREV
+			cp .Xresources ~/.Xresources	
 			cp -r i3/ ~/.config/i3/
 			cp .zshrc ~/.zshrc
 			echo 'You need to be superuser to restore those! (￣ω￣;)'
@@ -18,6 +19,7 @@ then
 		if ls ./backup/backup-$2 > /dev/null 2>&1
 		then
 			cd ./backup/backup-$2	
+			cp .Xresources ~/.Xresources
 			cp -r i3/ ~/.config/i3/
 			cp .zshrc ~/.zshrc
 			echo 'You need to be superuser to restore those! (￣ω￣;)'
@@ -29,7 +31,7 @@ then
 	fi
 else
 	echo 'hikkaCuteAF update tool. Note that this will replace all your files, but you can use restore.sh to restore from backup. Have fun! (^owo^)ﾉ' | lolcat
-	if [ $1 = "nobak" ]
+	if [ "$1" = "nobak" ]
 	then
 		echo 'Backup ignored.'
 	else
@@ -46,6 +48,7 @@ else
 	cp -r linuxowo/ ~/.oh-my-zsh/linuxowo/
 	cp -r i3/ ~/.config/i3/
 	cp .zshrc ~/.zshrc
+	cp .Xresources ~/.Xresources
 	echo 'You need to be superuser to update those! (￣ω￣;)' | lolcat
 	sudo cp i3status.conf /etc/
 	sudo cp zsh_command_not_found /etc/zsh_command_not_found
